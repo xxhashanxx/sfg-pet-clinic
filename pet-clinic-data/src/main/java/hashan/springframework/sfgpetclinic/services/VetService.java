05a26b3e4eb0f5388114df67d3ac4b4cd67f4fd7
malka liyanage <malka.liyanage96@gmail.com>
2021-04-12 18:30:02 +0530

    add Common Interface for Services. Closes #11

--- pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/services/VetService.java
@@ -9,8 +9,7 @@ import java.util.Set;
 /**
  * Created by jt on 7/18/18.
  */
-public interface VetService{
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
+
 }
+
