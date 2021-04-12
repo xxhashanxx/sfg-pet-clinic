211a328932fef3b4505a3f4528db74e2665f67cf
malka liyanage <malka.liyanage96@gmail.com>
2021-04-12 18:59:20 +0530

    Impl Map Base Services. Closes #6

---
+++ pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -0,0 +1,42 @@
+package hashan.springframework.sfgpetclinic.services.map;
+
+
+import hashan.springframework.sfgpetclinic.model.Pet;
+import hashan.springframework.sfgpetclinic.model.Vet;
+import hashan.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 7/21/18.
+ */
+@Service
+@Profile({"default", "map"})
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
