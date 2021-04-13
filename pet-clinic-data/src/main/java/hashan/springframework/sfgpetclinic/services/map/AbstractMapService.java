6318c872b5a8aa9d4b35517b7b39fe565122653c
malka liyanage <malka.liyanage96@gmail.com>
2021-04-13 20:03:48 +0530

    add owner list and update config. Closes #16, Closes #15, Closes #23

--- pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,14 +1,16 @@
 package hashan.springframework.sfgpetclinic.services.map;
 
 
+import hashan.springframework.sfgpetclinic.model.BaseEntity;
+
 import java.util.*;
 
 /**
  * Created by jt on 7/21/18.
  */
-public abstract class AbstractMapService<T,ID> {
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -18,8 +20,17 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id,T object){
-        map.put(id, object);
+    T save(T object){
+
+        if(object != null) {
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -31,5 +42,16 @@ public abstract class AbstractMapService<T,ID> {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
 
     }
+    private Long getNextId(){
 
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
+    }
 }
--- pet-clinic-web/src/main/java/hashan/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/hashan/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,5 +1,6 @@
 package hashan.springframework.sfgpetclinic.controllers;
 
+import hashan.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
-    public String listVets(){
-
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
