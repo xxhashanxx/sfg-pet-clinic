87d4d71ad77e509056b8ece20e591823fe64746c
malka liyanage <malka.liyanage96@gmail.com>
2021-06-30 11:32:35 +0530

    updated owners with jpa mapping

--- pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/model/Person.java
@@ -1,7 +1,13 @@
 package hashan.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+
 public class Person extends BaseEntity {
+
+    @Column(name ="first_name")
     private String firstName;
+
+    @Column(name ="last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/hashan/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,15 @@
 package hashan.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends  BaseEntity{
+
+    @Column(name = "name")
     private String  name;
 
     public String getName() {
